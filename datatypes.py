e66156a9927672705aaf0729908a590cbdc5af94
Akrathod007 <anshrathod2104@gmail.com>
2026-06-01 17:56:18 +0530

    Added

---
+++ datatypes.py
@@ -0,0 +1,114 @@
+no = 100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+print(no)
+print(type(no))
+
+pi = 3.14
+print(pi)
+print(type(pi))
+
+# 1 byte - 8 bit
+# 2^8 -> 256
+# -128 to 127
+
+# 8 byte -> 64 bit
+# 2 ^ 64
+
+isactive = True
+print(isactive)
+print(type(isactive))
+
+# name = "Hello"
+name = """Hello
+qwdnqw
+lnef
+kwnekf
+"""
+print(name)
+print(type(name))
+
+li = [1, 1.2, "hello", True]
+print(li)
+print(type(li))
+# name = 10
+# print(name)
+
+
+t = (1, 2, 3, 4)
+print(t)
+print(type(t))
+
+d = {"Name": "Ram", "Age": 21}
+print(d)
+print(type(d))
+
+s = {1, 2, 3, 2, 3, 4, 5}
+print(s)
+print(type(s))
+
+
+"""
+jbnefj
+nwkef
+knwe
+"""
+
+if 5 > 2:
+    print("Hi")
+    print("Bye")
+
+
+a, b, c = 10, "Hello", 2.3
+print(a)
+print(b)
+print(c)
+
+x = y = z = 100
+
+print(x)
+print(y)
+print(z)
+
+
+a = 10
+b = 20
+print(a)
+print(b)
+
+# c = a
+# a = b
+# b = c
+"""
+a = a + b  # a = 30
+b = a - b  # b = 10
+a = a - b  # a = 20
+
+"""
+
+# a, b = b, a
+# print(a)
+# print(b)
+
+"""
+a = "10"
+num = int(a)
+print(num)
+
+b = "1.2"
+num = float(b)
+print(b)
+
+c = 10
+c_str = str(c)
+print(c_str)
+
+
+
+"""
+
+No = 100
+
+print("Hello" + "World")
+print(10 + 20)
+print("Hello :" + str(No))
+
+print("No :", No)
---
+++ first.py
@@ -0,0 +1,27 @@
+print("Hello\nWorld")
+print("Bye\tBye")
+print("Good\b\bMorning")
+print("""Hello""")
+
+# 'Hello'
+# "Hello"
+# '''Hello'''
+# """Hello"""
+
+# 'Hello'
+print("'Hello'")
+# "Hello"
+print('"Hello"')
+# '''Hello'''
+print("'''Hello'''")
+# print(''''Hello'''')
+
+# 'Hello"
+print("'Hello\"")
+# \Hello\
+print("\Hello\\")
+
+print("\043")
+print("\101")
+
+print("\x4d")
