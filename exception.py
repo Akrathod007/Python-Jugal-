05e5b7079fab2ec308f823d50af76e0a513b16f6
Akrathod007 <anshrathod2104@gmail.com>
2026-07-16 16:28:12 +0530

    added

--- exception.py
+++ exception.py
@@ -41,15 +41,118 @@ else:
 
 """
 
-try:
-    print(10 / 0)
+# try:
+#     print(10 / 0)
 
-except:
-    print("Error")
+# except:
+#     print("Error")
 
-finally:
-    print("Always Executes")
+# finally:
+#     print("Always Executes")
 
 # print(x)
 
-import pandas
+
+age = 5
+
+if age < 0:
+    raise ValueError("Age can not be negative")
+
+print("Valid Age")
+
+
+balance = 500
+withdraw = 1000
+
+try:
+    if withdraw > balance:
+        raise Exception("Insufficient Balance")
+except Exception as e:
+    print("Error :", e)
+
+
+try:
+    age = int(input("Enter Age: "))
+
+    if age < 18:
+        raise ValueError("You are not eligible")
+
+    print("Eligible")
+
+except ValueError as e:
+    print("Error:", e)
+
+
+class InvalidMarksError(Exception):
+    pass
+
+
+marks = -10
+
+# if marks < 0:
+#     raise InvalidMarksError("Marks cannot be negative")
+
+try:
+    if marks < 0:
+        raise InvalidMarksError("Marks cannot be negative")
+except InvalidMarksError as e:
+    print("Error :", e)
+
+
+balance = 5000
+
+try:
+    amount = int(input("Enter withdrawal amount: "))
+
+    if amount > balance:
+        raise Exception("Insufficient Balance")
+
+    balance -= amount
+    print("Remaining Balance:", balance)
+
+except Exception as e:
+    print(e)
+
+
+try:
+    print("Outer Try")
+
+    try:
+        print(10 / 0)
+
+    except ZeroDivisionError:
+        print("Inner Except: Cannot divide by zero")
+
+except:
+    print("Outer Except")
+
+
+try:
+    print("Outer Try")
+
+    try:
+        num = int("abc")
+
+    except ZeroDivisionError:
+        print("Inner Except")
+
+except ValueError:
+    print("Outer Except: Invalid Number")
+
+
+try:
+    file = open("data.txt")
+
+    try:
+        data = int(file.read())
+        print(data)
+
+    except ValueError:
+        print("File contains invalid data")
+
+    finally:
+        file.close()
+        print("File Closed")
+
+except FileNotFoundError:
+    print("File Not Found")
